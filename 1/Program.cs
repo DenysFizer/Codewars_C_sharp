dff47b26d64ff736bd6768795087d002846c53c9
denfi <denfi@DENYS>
2022-02-05 15:28:55 +0200

    Add project files.

---
+++ 1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+public static class Kata
+{
+    public static void Main(string[] args)
+    {
+        int[] dice = new int[] { 4, 4, 4, 3, 3 };
+        Score(dice);
+    }
+    public static int Score(int[] dice)
+    {
+        int[] nums =new int[] {0, 0, 0, 0, 0, 0, 0};
+        for (int i = 0; i < dice.Length; i++)
+        {
+            nums[dice[i]]++;
+        }
+        int result = 0;
+      
+        for (int i = 1; i < nums.Length; i++)
+        {
+            switch (i)
+            {
+                case 1:
+
+                    if (nums[i] >= 3)
+                    {
+                        result += ((nums[i] / 3)*1000)+(nums[i]%3)*100;
+                    }
+                    else
+                    {
+                        result+= nums[i] * 100;
+                    }
+                        break;
+                case 2:
+                    if (nums[i] >= 3)
+                    {
+                        result += (nums[i] / 3) * 200;
+                    }
+                    break;
+                case 3:
+                    if (nums[i] >= 3)
+                    {
+                        result += (nums[i] / 3) * 300;
+                    }
+                    break;
+                case 4:
+                    if (nums[i] >= 3)
+                    {
+                       
+                        result += (nums[i] / 3) * 400;
+                    }
+                    break;
+                case 5:
+                    if (nums[i] >= 3)
+                    {
+                        result += ((nums[i] / 3) * 500) + (nums[i]%3)*50;
+                    }
+                    else
+                    {
+                        result += nums[i] * 50;
+                    }
+                    break;
+                case 6:
+                    if (nums[i] >= 3)
+                    {
+                        result += (nums[i] / 3) * 600;
+                    }
+                    break;
+            }
+        }
+
+        return result;
+
+    }}
